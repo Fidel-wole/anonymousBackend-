c4fc9afeedb3460b4918ac11fc3bab69747cb513
Adewole Adewoye <adewoyeadedayo10@gmail.com>
2023-10-12 20:25:27 +0100

    adjustment

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,31 +2,46 @@ const User = require('../model/user')
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken')
 
-exports.registerUser = (req, res, next)=>{
-const username = req.body.username;
-const password = req.body.password;
-User.findOne({username:username}).then(user =>{
-    if(user){
-       res.status(401).json({message:"Username already exist, please choose another one"}) 
-    }
-    return bcrypt.hash(password, 12);
-}).then((hashpassword)=>{
-    const user = new User({
-        username:username,
-        password:hashpassword,
+exports.registerUser = (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
 
+  // Check if the username already exists
+  User.findOne({ username: username })
+    .then((user) => {
+      if (user) {
+        // User with the given username already exists
+        return res.status(401).json({ message: "Username already exists, please choose another one" });
+      } else {
+        // Username is available, hash the password
+        return bcrypt.hash(password, 12);
+      }
+    })
+    .then((hashPassword) => {
+      // Create a new user with the hashed password
+      const user = new User({
+        username: username,
+        password: hashPassword,
+      });
 
+      // Save the user to the database
+      return user.save();
     })
-    user.save().then(()=>{
-        res.status(201).json({
-            message: "User Sucessfully Created",
-            details:{
-                username:username,
-            }
-        })
+    .then(() => {
+      // User successfully created
+      res.status(201).json({
+        message: "User Successfully Created",
+        details: {
+          username: username,
+        },
+      });
     })
-})
-}
+    .catch((error) => {
+      // Handle any errors that may occur during the registration process
+      console.error("Error registering user: ", error);
+      res.status(500).json({ message: "Internal server error" });
+    });
+};
 
 exports.login = (req, res, next)=>{
     const username = req.body.username;
